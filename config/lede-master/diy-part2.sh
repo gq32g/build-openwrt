97ec69bfe4563899b5521f1085063a91634c9b3a
gq32g <75679327+gq32g@users.noreply.github.com>
2026-02-14 18:09:32 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -33,7 +33,7 @@ sed -i 's/192.168.1.1/192.168.26.1/g' package/base-files/files/bin/config_genera
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
-# svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
+ svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
 
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
